dbe2774e74f97c28faf54376e7a79c681653fdb1
Josh Stoik <josh@blobfolio.com>
2017-05-04 11:15:00 -0700

    -- use namespace -- offload processing to class

--- wp/admin/debug.php
+++ wp/admin/debug.php
@@ -17,6 +17,10 @@ if (!defined('ABSPATH')) {
 	exit;
 }
 
+use \blobfolio\wp\bm\mime;
+use \blobfolio\wp\bm\admin;
+use \blobfolio\wp\bm\debug;
+
 if (!current_user_can('manage_options')) {
 	wp_die(__('You do not have sufficient permissions to access this page.'));
 }
@@ -24,67 +28,6 @@ if (!current_user_can('manage_options')) {
 if (!extension_loaded('fileinfo') || !defined('FILEINFO_MIME_TYPE')) {
 	echo '<div class="error fade"><p>The `fileinfo` PHP extension is not installed. Detailed file information is not available.</p></div>';
 	return true;
-}
-
-
-
-/**
- * Format Results
- *
- * @param  array|string $results Results.
- * @param int          $indent Indentation level.
- * @return string Results.
- */
-function blob_mimes_format_results($results, $indent = 0) {
-	$out = array();
-
-	if (is_array($results)) {
-		foreach ($results as $k=>$v) {
-			// The main reason is special.
-			if ('REASON' === $k) {
-				$out[] = "\n\n" . str_repeat('=', 25);
-				$out[] = 'REASON';
-				$out[] = str_repeat('=', 25);
-				$out[] = is_array($v) && count($v) ? $v[0] : 'N/A';
-				continue;
-			}
-
-			if (is_string($k)) {
-				if (is_array($v) && !count($v)) {
-					$v = 'N/A';
-				}
-
-				// Simple key/value pair.
-				if (is_string($v)) {
-					$out[] = str_repeat('    ', $indent) . "$k:" . str_repeat(' ', 12 - strlen($k)) . $v;
-					continue;
-				}
-
-				// New Section.
-				if (0 === $indent) {
-					$out[] = "\n\n" . str_repeat('-', 25);
-				}
-
-				$out[] = str_repeat('    ', $indent) . $k . ($indent > 0 ? ':' : '');
-
-				if (0 === $indent) {
-					$out[] = str_repeat('-', 25);
-				}
-			}
-
-			if (is_string($v)) {
-				$out[] = str_repeat('    ', $indent) . "•$v";
-			} else {
-				$tmp = blob_mimes_format_results($v, $indent + 1);
-				$tmp = explode("\n", $tmp);
-				foreach ($tmp as $t) {
-					$out[] = $t;
-				}
-			}
-		}
-	}
-
-	return implode("\n", $out);
 }
 
 
@@ -103,106 +46,11 @@ if ((getenv('REQUEST_METHOD') === 'POST') && wp_verify_nonce($_POST['n'], 'debug
 
 		if (!isset($_FILES['file']['tmp_name']) || !file_exists($_FILES['file']['tmp_name'])) {
 			$errors[] = 'No file was uploaded, or the upload was corrupt.';
-		} else {
-			// Start the output.
-			$results = array(
-				'REASON'=>array(),
-				'FILE'=>array(
-					'WORDPRESS'=>array(
-						'name'=>'',
-						'type'=>'',
-						'ext'=>'',
-					),
-					'FILEINFO'=>array(
-						'type'=>'',
-						'ext/type'=>'FALSE',
-					),
-					'BLOB-MIMES'=>array(
-						'name'=>'',
-						'type'=>'',
-						'ext'=>'',
-						'ext/type'=>'FALSE',
-					),
-				),
-				'ENVIRONMENT'=>array(
-					'WORDPRESS'=>array(
-						'version'=>get_bloginfo('version'),
-						'plugins'=>get_option('active_plugins'),
-						'theme'=>basename(get_stylesheet_directory()),
-					),
-					'PHP'=>array(
-						'version'=>phpversion(),
-						'OS'=>php_uname('a'),
-						'extensions'=>get_loaded_extensions(),
-					),
-				),
-			);
-			sort($results['ENVIRONMENT']['WORDPRESS']['plugins']);
-			sort($results['ENVIRONMENT']['PHP']['extensions']);
-
-			$file = $_FILES['file'];
-
-			// First, run a simple name check.
-			$results['FILE']['WORDPRESS']['name'] = $file['name'];
-			$info = wp_check_filetype($file['name']);
-			$results['FILE']['WORDPRESS']['ext'] = $info['ext'] ? $info['ext'] : 'FALSE';
-			$results['FILE']['WORDPRESS']['type'] = $info['type'] ? $info['type'] : 'FALSE';
-
-			// Failure by simple extension.
-			if ((false === $info['ext']) || (false === $info['type'])) {
-				$results['REASON'][] = "The uploaded file's name ends in an extension that is not part of the upload whitelist.";
-			}
-
-			// Now fileinfo.
-			$finfo = finfo_open(FILEINFO_MIME_TYPE);
-			$results['FILE']['FILEINFO']['type'] = finfo_file($finfo, $file['tmp_name']);
-			if (!is_string($results['FILE']['FILEINFO']['type']) || !strlen($results['FILE']['FILEINFO']['type'])) {
-				$results['FILE']['FILEINFO']['type'] = 'FALSE';
-			}
-
-			if ('FALSE' !== $results['FILE']['FILEINFO']['type']) {
-				$ext = pathinfo($file['name'], PATHINFO_EXTENSION);
-				$results['FILE']['FILEINFO']['ext/type'] = blobfolio\wp\bm\mime::check_alias($ext, $results['FILE']['FILEINFO']['type']) ? 'TRUE' : 'FALSE';
-			}
-
-			finfo_close($finfo);
-
-			// Now this plugin.
-			$info = blobfolio\wp\bm\mime::check_real_filetype($file['tmp_name'], $file['name']);
-			$results['FILE']['BLOB-MIMES']['ext'] = $info['ext'] ? $info['ext'] : 'FALSE';
-			$results['FILE']['BLOB-MIMES']['type'] = $info['type'] ? $info['type'] : 'FALSE';
-
-			// Reconstruct the file name.
-			if ((false !== $info['ext']) && (false !== $info['type'])) {
-				// Is this an alias?
-				$results['FILE']['BLOB-MIMES']['ext/type'] = blobfolio\wp\bm\mime::check_alias($results['FILE']['BLOB-MIMES']['ext'], $results['FILE']['BLOB-MIMES']['type']) ? 'TRUE' : 'FALSE';
-
-				// Reconstruct the name.
-				$filename_parts = explode('.', $file['name']);
-				array_pop($filename_parts);
-				$filename_parts[] = $results['FILE']['BLOB-MIMES']['ext'];
-				$results['FILE']['BLOB-MIMES']['name'] = implode('.', $filename_parts);
-			}
-
-			// Run it through the normal upload check to see what happens.
-			$info = blobfolio\wp\bm\admin::check_filetype_and_ext(null, $file['tmp_name'], $file['name'], get_allowed_mime_types());
-			if (!count($results['REASON'])) {
-				if ((false === $info['ext']) || (false === $info['type'])) {
-					if ('FALSE' !== $results['FILE']['BLOB-MIMES']['type']) {
-						if ('FALSE' !== $results['FILE']['BLOB-MIMES']['ext/type']) {
-							$results['REASON'][] = 'The MIME type matches the file extension, but that extension is not whitelisted.';
-						} else {
-							$results['REASON'][] = 'The MIME type is not (yet) known to match the file extension.';
-						}
-					} elseif ('FALSE' !== $results['FILE']['FILEINFO']['type']) {
-						$results['REASON'][] = 'Based on content, this file was found to not match any authorized types.';
-					} else {
-						$results['REASON'][] = 'Another plugin or script might be responsible for rejecting the file.';
-					}
-				} else {
-					$results['REASON'][] = 'This file passed the primary validation checks. :) If there is a problem, it is unfortunately somewhere else.';
-				}
-			}
+		}
+		// All the hard work is handled by the class.
+		else {
+			$debug = new debug($_FILES['file']['tmp_name'], $_FILES['file']['name']);
+			$results = $debug->get_results();
 
 			// We're done with the original file. Try to delete
 			// it just in case the request dies unexpectedly.
@@ -219,7 +67,8 @@ if ((getenv('REQUEST_METHOD') === 'POST') && wp_verify_nonce($_POST['n'], 'debug
 			echo '<div class="error fade"><p>' . esc_html($e) . '</p></div>';
 		}
 	}
-} elseif (isset($_POST['n'])) {
+}
+elseif (isset($_POST['n'])) {
 	echo '<div class="error fade"><p>The form had expired. Please try again.</p></div>';
 }
 
@@ -267,15 +116,15 @@ if ((getenv('REQUEST_METHOD') === 'POST') && wp_verify_nonce($_POST['n'], 'debug
 		<div id="post-body" class="meta-holder">
 			<div class="postbox-container">
 
-				<?php if (is_array($results)) { ?>
+				<?php if (!is_null($results)) { ?>
 					<!-- upload form -->
 					<div class="postbox">
 						<h3 class="hndle">Analysis</h3>
 						<div class="inside">
 
-							<p>If this file should have been accepted and wasn't, please submit the following information to the official <a href="https://core.trac.wordpress.org/ticket/39963" target="_blank">WordPress issue ticket</a>. Thanks!</p>
+							<p>If this file should have been accepted and wasn't, please open an issue with the following information at <a href="https://github.com/Blobfolio/blob-mimes/issues" target="_blank">Github</a> or, if you aren't comfortable posting there, email <a href="mailto:hello@blobfolio.com">hello@blobfolio.com</a> for help. Thanks!</p>
 
-							<textarea class="blob-mimes--results" onclick="this.select()"><?php echo esc_textarea(trim(blob_mimes_format_results($results))); ?></textarea>
+							<textarea class="blob-mimes--results" onclick="this.select()"><?php echo esc_textarea(trim($results)); ?></textarea>
 
 						</div><!--.inside-->
 					</div><!--.postbox-->
