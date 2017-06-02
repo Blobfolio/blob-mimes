a08e9986f121e0baccdcd0bf7cb5fa6381f887a9
Josh Stoik <josh@blobfolio.com>
2017-06-02 09:39:46 -0700

    expand unit tests

--- tests/test-mime.php
+++ tests/test-mime.php
@@ -17,129 +17,205 @@ class mime_tests extends \PHPUnit\Framework\TestCase {
 
 	const ASSETS = __DIR__ . '/assets/';
 
+	// --------------------------------------------------------------------
+	// Tests
+	// --------------------------------------------------------------------
+
 	/**
 	 * ::get_mime()
 	 *
-	 * @return void Nothing.
+	 * @dataProvider data_get_mime
+	 *
+	 * @param string $mime MIME.
+	 * @param string $expected_mime Expected MIME.
+	 * @param string $expected_ext Expected extension.
 	 */
-	function test_get_mime() {
-		$thing = mimes::get_mime('audio/mp3');
-		$this->assertEquals(true, is_array($thing));
-		$this->assertEquals('audio/mp3', $thing['mime']);
-		$this->assertEquals(true, in_array('mp3', $thing['ext'], true));
+	function test_get_mime($mime, $expected_mime, $expected_ext) {
+		$result = mimes::get_mime($mime);
+
+		$this->assertSame(true, is_array($result));
+		$this->assertEquals($expected_mime, $result['mime']);
+		$this->assertSame(true, in_array($expected_ext, $result['ext'], true));
 	}
 
 	/**
 	 * ::get_extension()
 	 *
-	 * @return void Nothing.
+	 * @dataProvider data_get_extension
+	 *
+	 * @param string $ext Extension.
+	 * @param string $expected_ext Expected extension.
+	 * @param string $expected_mime Expected MIME.
 	 */
-	function test_get_extension() {
-		$thing = mimes::get_extension('xls');
-		$this->assertEquals(true, is_array($thing));
-		$this->assertEquals('xls', $thing['ext']);
-		$this->assertEquals(true, in_array('application/vnd.ms-excel', $thing['mime'], true));
+	function test_get_extension($ext, $expected_ext, $expected_mime) {
+		$result = mimes::get_extension($ext);
+
+		$this->assertSame(true, is_array($result));
+		$this->assertEquals($expected_ext, $result['ext']);
+		$this->assertSame(true, in_array($expected_mime, $result['mime'], true));
 	}
 
 	/**
 	 * ::finfo()
 	 *
-	 * Test an actual file.
+	 * @dataProvider data_finfo
 	 *
-	 * @return void Nothing.
+	 * @param string $file File.
+	 * @param mixed $expected Expected.
+	 * @param mixed $suggestion Suggestion.
 	 */
-	function test_finfo_correct() {
-		$thing = mimes::finfo(self::ASSETS . 'space.jpg');
+	function test_finfo($file, $expected, $suggestion) {
+		$result = mimes::finfo($file);
+		$suggested = $result['suggested_filename'];
+		unset($result['suggested_filename']);
 
-		$this->assertEquals(true, is_array($thing));
-		$this->assertEquals('space.jpg', $thing['basename']);
-		$this->assertEquals('jpg', $thing['extension']);
-		$this->assertEquals('space', $thing['filename']);
-		$this->assertEquals('image/jpeg', $thing['mime']);
-		$this->assertEquals(0, count($thing['suggested_filename']));
-	}
-
-	/**
-	 * ::finfo()
-	 *
-	 * Test an actual file, incorrectly named.
-	 *
-	 * @return void Nothing.
-	 */
-	function test_finfo_incorrect() {
-		$thing = mimes::finfo(self::ASSETS . 'space.png');
-
-		$this->assertEquals(true, is_array($thing));
-		$this->assertEquals('space.png', $thing['basename']);
-		$this->assertEquals('jpeg', $thing['extension']);
-		$this->assertEquals('space', $thing['filename']);
-		$this->assertEquals('image/jpeg', $thing['mime']);
-		$this->assertEquals(true, in_array('space.jpg', $thing['suggested_filename'], true));
-	}
-
-	/**
-	 * ::finfo()
-	 *
-	 * Test a file name.
-	 *
-	 * @return void Nothing.
-	 */
-	function test_finfo_just_filename() {
-		$thing = mimes::finfo('pkcs12-test-keystore.tar.gz');
-
-		$this->assertEquals(true, is_array($thing));
-		$this->assertEquals('pkcs12-test-keystore.tar.gz', $thing['basename']);
-		$this->assertEquals('gz', $thing['extension']);
-		$this->assertEquals('pkcs12-test-keystore.tar', $thing['filename']);
-		$this->assertEquals('application/gzip', $thing['mime']);
-	}
-
-	/**
-	 * ::finfo()
-	 *
-	 * Test a remote file.
-	 *
-	 * @return void Nothing.
-	 */
-	function test_finfo_remote() {
-		$thing = mimes::finfo('https://upload.wikimedia.org/wikipedia/commons/7/76/Mozilla_Firefox_logo_2013.svg');
-
-		$this->assertEquals(true, is_array($thing));
-		$this->assertEquals('Mozilla_Firefox_logo_2013.svg', $thing['basename']);
-		$this->assertEquals('svg', $thing['extension']);
-		$this->assertEquals('Mozilla_Firefox_logo_2013', $thing['filename']);
-		$this->assertEquals('image/svg+xml', $thing['mime']);
-	}
-
-	/**
-	 * ::check_ext_and_mime()
-	 *
-	 * Test various XLS files.
-	 *
-	 * @return void Nothing.
-	 */
-	function test_check_ext_and_mime() {
-		if (
-			!function_exists('finfo_file') ||
-			!defined('FILEINFO_MIME_TYPE')
-		) {
-			$this->markTestSkipped('fileinfo.so not installed.');
+		$this->assertEquals($expected, $result);
+		if ($suggestion) {
+			$this->assertSame(true, in_array($suggestion, $suggested, true));
 		}
+	}
 
-		$things = array(
-			'xls-cdfv2.xls',
-			'xls-excel.xls',
-			'xls-msoffice.xls',
-			'xls-xml.xls'
+	// -------------------------------------------------------------------- end tests
+
+
+
+	// --------------------------------------------------------------------
+	// Data
+	// --------------------------------------------------------------------
+
+	/**
+	 * Data for ::get_mime()
+	 *
+	 * @return array Data.
+	 */
+	function data_get_mime() {
+		return array(
+			array(
+				'audio/Mp3',
+				'audio/mp3',
+				'mp3'
+			),
+			array(
+				'image/jpeg',
+				'image/jpeg',
+				'jpeg'
+			),
 		);
-
-		$finfo = finfo_open(FILEINFO_MIME_TYPE);
-		foreach ($things as $thing) {
-			$mime = finfo_file($finfo, self::ASSETS . $thing);
-			$this->assertEquals(true, mimes::check_ext_and_mime('xls', $mime));
-		}
-		finfo_close($finfo);
 	}
+
+	/**
+	 * Data for ::get_extension()
+	 *
+	 * @return array Data.
+	 */
+	function data_get_extension() {
+		return array(
+			array(
+				'mp3',
+				'mp3',
+				'audio/mp3'
+			),
+			array(
+				'Xls',
+				'xls',
+				'application/vnd.ms-office'
+			),
+			array(
+				'.XLS',
+				'xls',
+				'application/vnd.ms-excel'
+			),
+		);
+	}
+
+	/**
+	 * Data for ::finfo()
+	 *
+	 * @return array Data.
+	 */
+	function data_finfo() {
+		return array(
+			array(
+				static::ASSETS . 'space.jpg',
+				array(
+					'dirname'=>rtrim(static::ASSETS, '/'),
+					'basename'=>'space.jpg',
+					'extension'=>'jpg',
+					'filename'=>'space',
+					'path'=>static::ASSETS . 'space.jpg',
+					'mime'=>'image/jpeg',
+				),
+				null
+			),
+			// SVG w/ headers.
+			array(
+				static::ASSETS . 'blobfolio-type.svg',
+				array(
+					'dirname'=>rtrim(static::ASSETS, '/'),
+					'basename'=>'blobfolio-type.svg',
+					'extension'=>'svg',
+					'filename'=>'blobfolio-type',
+					'path'=>static::ASSETS . 'blobfolio-type.svg',
+					'mime'=>'image/svg+xml',
+				),
+				null
+			),
+			// SVG w/o headers.
+			array(
+				static::ASSETS . 'blobfolio-no_type.svg',
+				array(
+					'dirname'=>rtrim(static::ASSETS, '/'),
+					'basename'=>'blobfolio-no_type.svg',
+					'extension'=>'svg',
+					'filename'=>'blobfolio-no_type',
+					'path'=>static::ASSETS . 'blobfolio-no_type.svg',
+					'mime'=>'image/svg+xml',
+				),
+				null
+			),
+			// Incorrect name.
+			array(
+				static::ASSETS . 'space.png',
+				array(
+					'dirname'=>rtrim(static::ASSETS, '/'),
+					'basename'=>'space.png',
+					'extension'=>'jpeg',
+					'filename'=>'space',
+					'path'=>static::ASSETS . 'space.png',
+					'mime'=>'image/jpeg',
+				),
+				'space.jpg'
+			),
+			// Just a file name.
+			array(
+				'pkcs12-test-keystore.tar.gz',
+				array(
+					'dirname'=>getcwd(),
+					'basename'=>'pkcs12-test-keystore.tar.gz',
+					'extension'=>'gz',
+					'filename'=>'pkcs12-test-keystore.tar',
+					'path'=>getcwd() . '/pkcs12-test-keystore.tar.gz',
+					'mime'=>'application/gzip',
+				),
+				null
+			),
+			// Remote file.
+			array(
+				'https://upload.wikimedia.org/wikipedia/commons/7/76/Mozilla_Firefox_logo_2013.svg',
+				array(
+					'dirname'=>'https://upload.wikimedia.org/wikipedia/commons/7/76',
+					'basename'=>'Mozilla_Firefox_logo_2013.svg',
+					'extension'=>'svg',
+					'filename'=>'Mozilla_Firefox_logo_2013',
+					'path'=>'https://upload.wikimedia.org/wikipedia/commons/7/76/Mozilla_Firefox_logo_2013.svg',
+					'mime'=>'image/svg+xml',
+				),
+				null
+			),
+		);
+	}
+
+	// -------------------------------------------------------------------- end data
 }
 
 
