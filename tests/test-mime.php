56591d7b67cc1cb9bd5d120eb3c12088ec9b05f2
Josh Stoik <josh@blobfolio.com>
2018-05-02 22:45:14 -0700

    check str lock before and after tests

--- tests/test-mime.php
+++ tests/test-mime.php
@@ -8,6 +8,7 @@
  * @author	Blobfolio, LLC <hello@blobfolio.com>
  */
 
+use \blobfolio\common\constants;
 use \blobfolio\mimes\mimes;
 
 /**
@@ -16,6 +17,38 @@ use \blobfolio\mimes\mimes;
 class mime_tests extends \PHPUnit\Framework\TestCase {
 
 	const ASSETS = __DIR__ . '/assets/';
+
+
+
+	// -----------------------------------------------------------------
+	// Set up
+	// -----------------------------------------------------------------
+
+	/**
+	 * Before Test
+	 *
+	 * String cast bypass should be off before the test.
+	 *
+	 * @return void Nothing.
+	 */
+	protected function setUp() {
+		$this->assertFalse(constants::$str_lock);
+	}
+
+	/**
+	 * After Test
+	 *
+	 * String cast bypass should still be off after the test.
+	 *
+	 * @return void Nothing.
+	 */
+	protected function tearDown() {
+		$this->assertFalse(constants::$str_lock);
+	}
+
+	// ----------------------------------------------------------------- end setup
+
+
 
 	// --------------------------------------------------------------------
 	// Tests
