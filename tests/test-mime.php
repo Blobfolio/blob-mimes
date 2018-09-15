b294df7a004078c09c0af4c0078667c54ab2ccc3
Josh Stoik <josh@blobfolio.com>
2018-09-14 21:26:40 -0700

    remove str_lock test

--- tests/test-mime.php
+++ tests/test-mime.php
@@ -15,38 +15,7 @@ use \blobfolio\mimes\mimes;
  * Test Suite
  */
 class mime_tests extends \PHPUnit\Framework\TestCase {
-
 	const ASSETS = __DIR__ . '/assets/';
-
-
-
-	// -----------------------------------------------------------------
-	// Set up
-	// -----------------------------------------------------------------
-
-	/**
-	 * Before Test
-	 *
-	 * String cast bypass should be off before the test.
-	 *
-	 * @return void Nothing.
-	 */
-	protected function setUp() {
-		$this->assertFalse(constants::$str_lock);
-	}
-
-	/**
-	 * After Test
-	 *
-	 * String cast bypass should still be off after the test.
-	 *
-	 * @return void Nothing.
-	 */
-	protected function tearDown() {
-		$this->assertFalse(constants::$str_lock);
-	}
-
-	// ----------------------------------------------------------------- end setup
 
 
 
